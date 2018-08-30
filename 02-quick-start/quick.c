7fead3592abcca24d1fe20913e0dfdda9ee24972
jaeyoung.choi <lactrious@gmail.com>
2018-08-30 10:37:23 +0900

    WIP

--- 02-quick-start/quick.c
+++ 02-quick-start/quick.c
@@ -1,6 +1,6 @@
-#include <glad/glad.h>
+// #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include "linmath.h"
+// #include "linmath.h"
 #include <stdlib.h>
 #include <stdio.h>
 static const struct
